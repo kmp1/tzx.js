012d90e315ef3af900bc1be495759cb07bde1e87
kmp1 <kevinphil@hotmail.com>
2015-02-14 19:01:52 +0100

    Better commentin

--- src/tzx.js
+++ src/tzx.js
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
 var tzx_js = (function () {
 
     "use strict";
@@ -282,6 +283,8 @@ var tzx_js = (function () {
             blocks: retBlockDetails
         };
     }
+
+    ////////// TAP Data Reading Functions
 
     function readTapData(input, i, machineSettings, output, blockDetails) {
         var pilotLength, dataStart = i + 2;
